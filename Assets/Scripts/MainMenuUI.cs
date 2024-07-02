697a795e185a61580ec9a63658e8c1a621293504
ZeroTKA <zerotka@gmail.com>
2024-07-01 19:39:11 -0600

    Quit Button
    THe Quite button works but this is more of a test for committing to the side branch.

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Quit()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
     }
 }
