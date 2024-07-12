4c3e42b716a79f31a031cee5673c3368e8fa3d89
ZeroTKA <zerotka@gmail.com>
2024-07-11 20:27:10 -0600

    Basic Spawning Works.
    Not hooked up to pool system yet.

--- Assets/Scripts/Wave Manager.cs
+++ Assets/Scripts/Wave Manager.cs
@@ -6,6 +6,22 @@ using UnityEngine;
 public class WaveManager : MonoBehaviour
 {
     public Wave[] waves;
+    public Transform[] spawnPoints;
+
+    private Wave currentWave;
+    private int currentWaveNumber;
+
+    private void Update()
+    {
+        currentWave = waves[currentWaveNumber];
+        SpawnWave();
+    }
+    void SpawnWave()
+    {
+        GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+        Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(randomEnemy, randomSpawnPoint.position, Quaternion.identity);
+    }
 }
 
 [System.Serializable]
