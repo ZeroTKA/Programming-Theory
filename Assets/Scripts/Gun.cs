402064d9a11d2f1e64babd1bc4ccb973a6183583
ZeroTKA <zerotka@gmail.com>
2024-07-14 21:22:29 -0600

    Final for Branch
    Basics of Wave Manager implemented.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,3 +1,4 @@
+using OpenCover.Framework.Model;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -24,22 +25,21 @@ public class Gun : MonoBehaviour
 
     public void Shooting()
     {
-        
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-           Debug.DrawRay(fpsCam.transform.position, fpsCam.transform.forward * hit.distance, Color.yellow);
+            Debug.DrawRay(fpsCam.transform.position, fpsCam.transform.forward * hit.distance, Color.yellow);
 
             //vfx hookup
             //GameObject a = Instantiate(FireVFX, firePoint.position , Quaternion.identity);
             //GameObject b = Instantiate(HitVFX, hit.point, Quaternion.identity);
 
-           Runner script = hit.transform.gameObject.GetComponent<Runner>();
-           
-           if (script != null ) 
-           {                
-               script.ChangeHealth(-2);
-           }
+            Enemy script = hit.transform.gameObject.GetComponent<Enemy>();
+            if (script != null)
+            {
+                script.ChangeHealth(-2);
+            }
         }
     }
 }
